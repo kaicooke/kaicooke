c1a692dcfc96cd37a17fb85f8a15ed04492af503
Jesse <robert@prospecting.in>
2021-06-06 14:20:56 +0700

    cc

--- js/tmp.js
+++ js/tmp.js
@@ -25,42 +25,43 @@ for (var i = 0; i < links.length; i++) {
 sn leads
 
 */
-window.scrollTo(0, document.body.scrollHeight);
-setTimeout(function() {
-    var leads = document.querySelectorAll('.result-lockup > div.result-lockup__profile-info.flex.flex-column > div > dl');
-    for (var i = 0; i < leads.length; i++) {
-        let lead = leads[i];
-        if (lead) {
-            let name = lead.querySelector('.result-lockup__name').innerText;
-            let format = false;
-            let names = name.split(" ");
-            if (names.length == 2) {
-                name = "\t" + names[0] + "\t" + names[1]
-                format = true;
-            }
-            if (names.length == 3) {
-                name = "\t" + names[0] + "\t" + names[2]
-                format = true;
-            }
-            if (format) {
-                let url = lead.querySelector('.result-lockup__name a').href.trim();
-                let position = lead.querySelector('.result-lockup__highlight-keyword > span').innerText.trim();
-                let company = lead.querySelector('.result-lockup__position-company > a > span').innerText.trim();
-                let yearsExperience = lead.querySelector('dd:nth-child(4)').innerText.trim();
-                let yearsInPosition = yearsExperience.split(" ");
-                let yearsInCurrentPosition = 0;
-                if (yearsInPosition.length > 1) {
-                    yearsInCurrentPosition = yearsInPosition[0];
+var out = function() {
+    window.scrollTo(0, document.body.scrollHeight);
+    setTimeout(function() {
+        var leads = document.querySelectorAll('.result-lockup > div.result-lockup__profile-info.flex.flex-column > div > dl');
+        for (var i = 0; i < leads.length; i++) {
+            let lead = leads[i];
+            if (lead) {
+                let name = lead.querySelector('.result-lockup__name').innerText;
+                let format = false;
+                let names = name.split(" ");
+                if (names.length == 2) {
+                    name = "\t" + names[0] + "\t" + names[1]
+                    format = true;
                 }
+                if (names.length == 3) {
+                    name = "\t" + names[0] + "\t" + names[2]
+                    format = true;
+                }
+                if (format) {
+                    let url = lead.querySelector('.result-lockup__name a').href.trim();
+                    let position = lead.querySelector('.result-lockup__highlight-keyword > span').innerText.trim();
+                    let company = lead.querySelector('.result-lockup__position-company > a > span').innerText.trim();
+                    let yearsExperience = lead.querySelector('dd:nth-child(4)').innerText.trim();
+                    let yearsInPosition = yearsExperience.split(" ");
+                    let yearsInCurrentPosition = 0;
+                    if (yearsInPosition.length > 1) {
+                        yearsInCurrentPosition = yearsInPosition[0];
+                    }
 
-                let location = lead.querySelector('dd:nth-child(5)').innerText.trim();
-                console.log("\t" + name + "\t" + position + "\t" + company + "\t" + url + "\t" + yearsInCurrentPosition + "\t" + yearsExperience + "\t" + location)
-                    //"\t" + name + "\t" + position + "\t" + company + "\t" + url + "\t" + yearsInCurrentPosition + "\t" + yearsExperience + "\t" + location
+                    let location = lead.querySelector('dd:nth-child(5)').innerText.trim();
+                    console.log("\t" + name + "\t" + position + "\t" + company + "\t" + url + "\t" + yearsInCurrentPosition + "\t" + yearsExperience + "\t" + location)
+                        //"\t" + name + "\t" + position + "\t" + company + "\t" + url + "\t" + yearsInCurrentPosition + "\t" + yearsExperience + "\t" + location
+                }
             }
         }
-    }
-}, 2000);
-
+    }, 2000);
+}
 
 
 
